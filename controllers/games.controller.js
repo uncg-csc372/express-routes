717200b9580970ae4bc5b9f3b9f965183e528167
csc340sp24 <csc340sp24@gmail.com>
2024-10-23 22:11:52 -0400

    Added project files.

---
+++ controllers/games.controller.js
@@ -0,0 +1,75 @@
+"use strict";
+const express = require("express");
+const app = express();
+
+const multer = require("multer");
+app.use(multer().none());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const model = require("../models/games.model");
+
+function getAll(req, res, next) {
+  try {
+    res.json(model.getAll());
+  } catch (err) {
+    console.error("Error while getting games ", err.message);
+    next(err);
+  }
+}
+
+function getAllByOneAttribute(req, res, next) {
+  let attribute = req.query.attribute;
+  let value = req.query.value;
+  if (attribute && value) {
+    try {
+      res.json(model.getAllByOneAttribute(attribute, value));
+    } catch (err) {
+      console.error("Error while getting games: ", err.message);
+      next(err);
+    }
+  }
+  else {
+    res.status(400).send("Invalid Request");
+  }
+}
+
+function getOneById(req, res, next) {
+  try {
+    res.json(model.getOneById(req.params.id));
+  } catch (err) {
+    console.error("Error while getting games: ", err.message);
+    next(err);
+  }
+}
+
+function createNew(req, res, next) {
+  let id = parseInt(req.body.id);
+  let name = req.body.name;
+  let platform = req.body.platform;
+  let release_year = parseInt(req.body.release_year);
+  let genre = req.body.genre;
+  let publisher = req.body.publisher;
+  let developer = req.body.developer;
+  let rating = req.body.rating;
+
+  if (id && name && platform && release_year && genre && publisher && developer && rating) {
+    let params = [id, name, platform, release_year, genre, publisher, developer, rating];
+    try {
+      res.json(model.createNew(params));
+    } catch (err) {
+      console.error("Error while creating game: ", err.message);
+      next(err);
+    }
+  }
+  else {
+    res.status(400).send("Invalid Request");
+  }
+}
+
+module.exports = {
+  getAll,
+  getAllByOneAttribute,
+  getOneById,
+  createNew
+};
---
+++ server.js
@@ -0,0 +1,28 @@
+"use strict";
+const express = require("express");
+const app = express();
+
+const gamesRoutes = require("./routes/games.route");
+const { db_close } = require("./models/db-conn");
+
+app.use(express.static("public"));
+app.use("/games", gamesRoutes);
+
+app.get("/", (req, res) => {
+  res.redirect("/games/all");
+});
+
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, function () {
+  console.log("App listening at http://localhost:" + PORT);
+});
+
+process.on("SIGINT", cleanUp);
+function cleanUp() {
+  console.log("Terminate signal received.");
+  db_close();
+  console.log("...Closing HTTP server.");
+  server.close(() => {
+    console.log("...HTTP server closed.")
+  })
+}
