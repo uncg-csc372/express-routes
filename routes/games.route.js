36547897157a90a01c70c0f728f87c8e39926d2a
uncg-csc372 <156245488+uncg-csc372@users.noreply.github.com>
2024-11-15 19:59:49 -0500

    Update games.route.js

--- routes/games.route.js
+++ routes/games.route.js
@@ -1,6 +1,15 @@
 "use strict";
 const express = require("express");
 const router = express.Router();
+const cors = require('cors');
+
+const corsOptions = {
+  origin: 'http://localhost:3001', // frontend URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true // Allow cookies and authentication headers
+};
+
+router.use(cors(corsOptions));
 
 const gamesController = require("../controllers/games.controller");
 
